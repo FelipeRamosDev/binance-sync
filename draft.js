e744cab385e66dd9023b67b1ece002a3171c49bc
FelipeRamosDev <felipe@feliperamos.info>
2024-02-04 05:44:59 -0300

    Update draft.js

--- draft.js
+++ draft.js
@@ -1,8 +1,2 @@
 const BinanceSync = require('./index');
 const binance = new BinanceSync();
-
-require('mdoc').run({
-    // configuration options (specified below)
-    inputDir: 'src',
-    outputDir: 'docs'
-});
