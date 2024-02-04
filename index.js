9c64cf8499cb12674176b57ea47ad3797c53768e
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-02-04 05:40:53 -0300

    [BNB-15] Fix the missing documentation pages (#14)
    ## [BNB-15] Fix the missing documentation pages

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const BinanceSync  = require('./src/BinanceSync');
 
 /**
  * BinanceAJAX is a class for managing AJAX requests to Binance.
+ * @type {BinanceAJAX}
  */
 BinanceSync.BinanceAJAX = BinanceAJAX;
 
 /**
  * BinanceStreams is a class for managing WebSocket streams on Binance API.
+ * @type {BinanceStreams}
  */
 BinanceSync.BinanceStreams = BinanceStreams;
 
 /**
  * BinanceWS is a class for managing WebSocket connections on Binance API.
+ * @type {BinanceWS}
  */
 BinanceSync.BinanceWS = BinanceWS;
 
 /**
  * BinanceSync is a class for synchronizing with Binance API.
+ * @type {BinanceSync}
  */
 module.exports = BinanceSync;
