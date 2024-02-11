da65b6bc5d6532026f4ec37913b0d494abd27f51
FelipeRamosDev <felipe@feliperamos.info>
2024-02-11 15:57:29 -0300

    [BNB-24] Adjustments needed during implementation on bot-store
    ## [BNB-24] Adjustments needed during implementation on bot-store

--- src/BinanceWS.js
+++ src/BinanceWS.js
@@ -1,3 +1,4 @@
+const WebSocket  = require('ws');
 const appConfigs  = require('../configs.json');
 
 /**
@@ -57,25 +58,6 @@ class BinanceWS {
             throw err;
         }
     }
-
-    /**
-     * Subscribes to a WebSocket stream.
-     * @async
-     * @param {Object} params - The parameters for the subscription.
-     * @return {Promise<WebSocket>} The WebSocket object with the connection.
-     * @throws {Error} If there is an error during the request.
-     */
-    async subscribe(params) {
-        try {
-            if (window) {
-                return await this.clientSubscribe(params);
-            } else {
-                return await this.serverSubscribe(params);
-            }
-        } catch (err) {
-            throw new Error.Log(err);
-        }
-    }
     
     /**
      * Subscribes to a WebSocket stream.
@@ -84,8 +66,7 @@ class BinanceWS {
      * @return {Promise<WebSocket>} The WebSocket object with the connection.
      * @throws {Error} If there is an error during the request.
      */
-    async serverSubscribe(params) {
-        const WebSocket  = require('ws');
+    async subscribe(params) {
         const { endpoint, isPublic, callbacks } = Object(params);
         const { open, error, data, close } = Object(callbacks);
         let endpointAppend;
@@ -135,57 +116,6 @@ class BinanceWS {
             throw err;
         }
     }
-
-    async clientSubscribe(params) {
-        const { endpoint, isPublic, callbacks } = Object(params);
-        const { open, error, data, close } = Object(callbacks);
-        let endpointAppend;
-    
-        try {
-            if (!isPublic) {
-                const listenKey = await this.getListenKey();
-                endpointAppend = `${this.baseURL}/${listenKey}${endpoint || ''}`;
-            } else {
-                endpointAppend = `${this.baseURL}/${endpoint || ''}`;
-            }
-    
-            const ws = new window.WebSocket(endpointAppend); // Use the native WebSocket object
-    
-            ws.onopen = () => {
-                if (typeof open === 'function') {
-                    open();
-                }
-            };
-    
-            ws.onerror = (err) => {
-                if (typeof error !== 'function') {
-                    return;
-                }
-    
-                if (typeof err === 'string') {
-                    error(JSON.parse(err));
-                } else {
-                    error(err);
-                }
-            };
-    
-            ws.onmessage = (input) => {
-                if (typeof data === 'function') {
-                    data(JSON.parse(input.data)); // Use the 'data' property of the input object
-                }
-            };
-    
-            ws.onclose = () => {
-                if (typeof close === 'function') {
-                    close();
-                }
-            };
-    
-            return ws;
-        } catch (err) {
-            throw err;
-        }
-    }    
 }
 
 module.exports = BinanceWS;
