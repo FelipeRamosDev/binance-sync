dd696dd2d246ffa9fd3db8df27951adcde85689e
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-03-17 11:53:11 -0300

    [BNB-37] Implement a reconnect approach for userData (#40)
    * [BNB-37] Implement a reconnect approach for userData

--- src/BinanceWS.js
+++ src/BinanceWS.js
@@ -73,51 +73,58 @@ class BinanceWS {
      * Subscribes to a WebSocket stream.
      * @async
      * @param {Object} params - The parameters for the subscription.
+     * @param {string} params.endpoint - Binance endpoint, the same you get on Binance API documentation.
+     * @param {boolean} params.isPublic - If the subscription is a public route, if set to true, it won't send the KEYS on the request headers
+     * @param {Function} params.onOpen - The callback function to execute when it's opened, it receive the Websocket connection as the first parameter.
+     * @param {Function} params.onError - The callback function to execute when an error occur, it receive the error object as the first parameter.
+     * @param {Function} params.onData - The callback function to execute when a data is received, it receive the data object as the first parameter.
+     * @param {Function} params.onClose - The callback function to execute when is opened, it receive the Websocket connection as the first parameter.
      * @return {Promise<WebSocket>} The WebSocket object with the connection.
      * @throws {Error} If there is an error during the request.
      */
     async subscribe(params) {
-        const { endpoint, isPublic, callbacks } = Object(params);
-        const { open, error, data, close } = Object(callbacks);
+        const { endpoint, isPublic, onOpen, onError, onData, onClose } = Object(params);
         let endpointAppend;
+        let listenKey;
 
         try {
             if (!isPublic) {
-                const listenKey = await this.getListenKey();
+                listenKey = await this.getListenKey();
                 endpointAppend = `${this.baseURL}/${listenKey}${endpoint || ''}`;
             } else {
                 endpointAppend = `${this.baseURL}/${endpoint || ''}`;
             }
 
             const ws = new WebSocket(endpointAppend);
+            ws.listenKey = listenKey;
 
             ws.on('open', () => {
-                if (typeof open === 'function') {
-                    open();
+                if (typeof onOpen === 'function') {
+                    onOpen(ws);
                 }
             });
 
             ws.on('error', (err) => {
-                if (typeof error !== 'function') {
+                if (typeof onError !== 'function') {
                     return;
                 }
 
                 if (typeof err === 'string') {
-                    error(JSON.parse(err));
+                    onError(JSON.parse(err));
                 } else {
-                    error(err);
+                    onError(err);
                 }
             });
 
             ws.on('message', (input) => {
-                if (typeof data === 'function') {
-                    data(JSON.parse(input));
+                if (typeof onData === 'function') {
+                    onData(JSON.parse(input));
                 }
             });
 
             ws.on('close', () => {
-                if (typeof close === 'function') {
-                    close();
+                if (typeof onClose === 'function') {
+                    onClose(ws);
                 }
             });
 
