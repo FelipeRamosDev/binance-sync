2d4e567a2ed83970da9f5e68dd043c39a174c7f2
FelipeRamosDev <felipe@feliperamos.info>
2024-05-11 13:51:25 -0300

    [BNB-49] Create dynamic listeners attachment for both sides
    ## [BNB-49] Create dynamic listeners attachment for both sides

--- src/globals.js
+++ src/globals.js
@@ -1,16 +1,21 @@
-const { isClient } = require('./helpers');
+const { isClient, appendEvent, emitEvent } = require('./helpers');
 
 // Helpers
 if (isClient()) {
     window.isClient = isClient;
+    window.appendEvent = appendEvent;
+    window.emitEvent = emitEvent;
 } else {
     global.isClient = isClient;
+    global.appendEvent = appendEvent;
+    global.emitEvent = emitEvent;
 }
 
 if (!global.binanceSync) {
     global.binanceSync = {
         charts: {},
-        userDataStream: {}
+        userDataStream: {},
+        clientEvents: {}
     };
 
     Error.new = (name, message, code) => {
