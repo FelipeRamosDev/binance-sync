6e5937b170b8143ea60d55b77dedc4f66fc2c665
FelipeRamosDev <felipe@feliperamos.info>
2024-06-18 20:40:38 -0300

    [BNB-57] Update documentation
    ## [BNB-57] Description
    Updating the documentation

--- src/models/AccountTrade.js
+++ src/models/AccountTrade.js
@@ -1,4 +1,27 @@
+/**
+ * @class
+ * Represents an account trade.
+ */
 class AccountTrade {
+    /**
+     * Constructs a new AccountTrade instance.
+     * @param {Object} setup - The setup object containing trade details.
+     * @param {string} setup.symbol - The symbol of the trade (e.g., BTCUSDT).
+     * @param {number} setup.id - The ID of the trade.
+     * @param {number} setup.orderId - The order ID associated with the trade.
+     * @param {string} setup.side - The side of the trade (e.g., BUY, SELL).
+     * @param {number} setup.price - The price at which the trade was executed.
+     * @param {number} setup.qty - The quantity of the trade.
+     * @param {number} setup.realizedPnl - The realized profit and loss of the trade.
+     * @param {string} setup.marginAsset - The margin asset of the trade.
+     * @param {number} setup.quoteQty - The quote quantity of the trade.
+     * @param {number} setup.commission - The commission for the trade.
+     * @param {string} setup.commissionAsset - The commission asset for the trade.
+     * @param {number} setup.time - The time the trade was executed.
+     * @param {string} setup.positionSide - The position side of the trade (e.g., LONG, SHORT).
+     * @param {boolean} setup.buyer - Indicates if the trade was a buyer trade.
+     * @param {boolean} setup.maker - Indicates if the trade was a maker trade.
+     */
     constructor(setup) {
         const {
             symbol,
@@ -18,23 +41,102 @@ class AccountTrade {
             maker
         } = Object(setup);
 
+        /**
+         * @type {string}
+         * @description The symbol of the trade (e.g., BTCUSDT).
+         */
         this.symbol = symbol;
-        this.id = id
+        
+        /**
+         * @type {number}
+         * @description The ID of the trade.
+         */
+        this.id = id;
+        
+        /**
+         * @type {number}
+         * @description The order ID associated with the trade.
+         */
         this.orderId = orderId;
+        
+        /**
+         * @type {string}
+         * @description The side of the trade (e.g., BUY, SELL).
+         */
         this.side = side;
-        this.price = this.parseNum(price),
-        this.qty = this.parseNum(qty),
-        this.realizedPnl = this.parseNum(realizedPnl),
+        
+        /**
+         * @type {number}
+         * @description The price at which the trade was executed.
+         */
+        this.price = this.parseNum(price);
+        
+        /**
+         * @type {number}
+         * @description The quantity of the trade.
+         */
+        this.qty = this.parseNum(qty);
+        
+        /**
+         * @type {number}
+         * @description The realized profit and loss of the trade.
+         */
+        this.realizedPnl = this.parseNum(realizedPnl);
+        
+        /**
+         * @type {string}
+         * @description The margin asset of the trade.
+         */
         this.marginAsset = marginAsset;
-        this.quoteQty = this.parseNum(quoteQty),
-        this.commission = this.parseNum(commission),
+        
+        /**
+         * @type {number}
+         * @description The quote quantity of the trade.
+         */
+        this.quoteQty = this.parseNum(quoteQty);
+        
+        /**
+         * @type {number}
+         * @description The commission for the trade.
+         */
+        this.commission = this.parseNum(commission);
+        
+        /**
+         * @type {string}
+         * @description The commission asset for the trade.
+         */
         this.commissionAsset = commissionAsset;
+        
+        /**
+         * @type {number}
+         * @description The time the trade was executed.
+         */
         this.time = time;
+        
+        /**
+         * @type {string}
+         * @description The position side of the trade (e.g., LONG, SHORT).
+         */
         this.positionSide = positionSide;
+        
+        /**
+         * @type {boolean}
+         * @description Indicates if the trade was a buyer trade.
+         */
         this.buyer = buyer;
+        
+        /**
+         * @type {boolean}
+         * @description Indicates if the trade was a maker trade.
+         */
         this.maker = maker;
     }
 
+    /**
+     * Parses a value to a number.
+     * @param {*} value - The value to be parsed.
+     * @returns {number} - The parsed number, or undefined if the value is not a number.
+     */
     parseNum(value) {
         if (isNaN(value)) {
             return;
--- src/models/userDataEvents/UserDataPosition.js
+++ src/models/userDataEvents/UserDataPosition.js
@@ -1,6 +1,28 @@
 const UserDataPositionBase = require('./UserDataPositionBase');
 
+/**
+ * Represents a user data position.
+ * @class UserDataPosition
+ */
 class UserDataPosition extends UserDataPositionBase {
+    /**
+     * Creates an instance of UserDataPosition.
+     * @param {Object} setup - The setup object containing user data position properties.
+     * @param {number} setup.entryPrice - The entry price of the position.
+     * @param {string} setup.marginType - The margin type associated with the position.
+     * @param {boolean} setup.isAutoAddMargin - Indicates if auto-add margin is enabled.
+     * @param {number} setup.isolatedMargin - The isolated margin value.
+     * @param {number} setup.leverage - The leverage of the position.
+     * @param {number} setup.liquidationPrice - The liquidation price of the position.
+     * @param {number} setup.markPrice - The mark price of the position.
+     * @param {number} setup.maxNotionalValue - The maximum notional value of the position.
+     * @param {string} setup.symbol - The symbol associated with the position.
+     * @param {number} setup.unRealizedProfit - The unrealized profit of the position.
+     * @param {string} setup.positionSide - The position side (e.g., "long" or "short").
+     * @param {number} setup.notional - The notional value of the position.
+     * @param {number} setup.updateTime - The time when the position was last updated.
+     * @throws {Error} Throws an error if there is an issue during setup.
+     */
     constructor(setup) {
         super(setup);
         const {
@@ -20,18 +42,83 @@ class UserDataPosition extends UserDataPositionBase {
         } = Object(setup);
 
         this.type = 'UserDataPosition';
+
+        /**
+         * The entry price of the position.
+         * @member {number}
+         */
         this.entryPrice = parseNum(entryPrice);
+
+        /**
+         * The margin type associated with the position.
+         * @member {string}
+         */
         this.marginType = marginType;
+
+        /**
+         * Indicates if auto-add margin is enabled.
+         * @member {boolean}
+         */
         this.isAutoAddMargin = isAutoAddMargin;
+
+        /**
+         * The isolated margin value.
+         * @member {number}
+         */
         this.isolatedMargin = parseNum(isolatedMargin);
+
+        /**
+         * The leverage of the position.
+         * @member {number}
+         */
         this.leverage = parseNum(leverage);
+
+        /**
+         * The liquidation price of the position.
+         * @member {number}
+         */
         this.liquidationPrice = parseNum(liquidationPrice);
+
+        /**
+         * The mark price of the position.
+         * @member {number}
+         */
         this.markPrice = parseNum(markPrice);
+
+        /**
+         * The maximum notional value of the position.
+         * @member {number}
+         */
         this.maxNotionalValue = parseNum(maxNotionalValue);
+
+        /**
+         * The symbol associated with the position.
+         * @member {string}
+         */
         this.symbol = symbol;
+
+        /**
+         * The unrealized profit of the position.
+         * @member {number}
+         */
         this.unRealizedProfit = parseNum(unRealizedProfit);
+
+        /**
+         * The position side (e.g., "long" or "short").
+         * @member {string}
+         */
         this.positionSide = positionSide;
+
+        /**
+         * The notional value of the position.
+         * @member {number}
+         */
         this.notional = parseNum(notional);
+
+        /**
+         * The time when the position was last updated.
+         * @member {number}
+         */
         this.updateTime = updateTime;
     }
 }
